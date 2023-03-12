d7777b33d4cd9e5b39b7bb3e532faed5b14d0d06
kaleed-41 <danielwil621@gmail.com>
2023-03-11 23:02:22 -0400

    Added validation for email, password and confirm-password

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // make sentence title case
 function titleCase(string)
 {
@@ -43,7 +45,6 @@ function validateForm()
 {
     inputs.forEach(input => {
         // input.style.border = '1px solid hsl(223, 7%, 63%)';
-
         let nameAttribute = input.getAttribute('name');
         input.value.trim();
         if(input.value === '')
@@ -65,14 +66,14 @@ function validateForm()
         {
 
             // console.log(nameAttribute);
-            if(nameAttribute == 'first-name')
+            if(nameAttribute === 'first-name')
             {
                 // replace - in name attribute with ' '
                 nameAttribute = nameAttribute.replace('-', ' ');
 
                 // check length of value in input field
                 // console.log(`inputname: ${nameAttribute}`, `input value length: ${input.value.length}`);
-                if(input.value.length > 5 && input.value.length < 20)
+                if(input.value.length > 3 && input.value.length < 20)
                 {
                     // console.log('first name between 5 and 20 chars');
                     displaySuccess(input, input.nextElementSibling, '');
@@ -80,14 +81,14 @@ function validateForm()
                 else
                 {
                     // console.log('first name not between 5 and 20 chars');
-                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} must be between 5 and 20 characters`);
+                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} must be between 3 and 20 characters`);
                 }
             }
 
-
-            else if(nameAttribute == 'last-name')
+            else if(nameAttribute === 'last-name')
             {
-                if(input.value.length > 5 && input.value.length < 20)
+                nameAttribute = nameAttribute.replace('-', ' ');
+                if(input.value.length > 3 && input.value.length < 20)
                 {
                     // console.log(`${nameAttribute} between 5 and 20 chars`);
                     displaySuccess(input, input.nextElementSibling, '');
@@ -95,11 +96,76 @@ function validateForm()
                 else
                 {
                     // console.log(`${nameAttribute} between 5 and 20 chars`);
-                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} must be between 5 and 20 characters`);
+                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} must be between 3 and 20 characters`);
                 }
             }
 
+            else if(nameAttribute === 'email')
+            {
+                if(input.value.includes('@') && input.value.endsWith('.com'))
+                {
+                    displaySuccess(input, input.nextElementSibling, '');
+                }
+                else
+                {
+                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} not valid`);
+                }
+            }
 
+            // NOT WORKING
+            else if(nameAttribute === 'phone-number')
+            {
+                nameAttribute = nameAttribute.replace('-', ' ');
+                const regEx = '/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im';
+                if(input.value.match(regEx))
+                {
+                    displaySuccess(input, input.nextElementSibling, '');
+                }
+                else
+                {
+                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} not valid`);
+                }
+            }
+
+            else if(nameAttribute === 'password')
+            {
+                if(input.value.length < 5)
+                {
+                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} must be more than 5 characters`);
+                }
+                else
+                {
+                    displaySuccess(input, input.nextElementSibling, '');
+                }
+            }
+
+            else if(nameAttribute === 'confirm-password')
+            {
+                nameAttribute = nameAttribute.replace('-', ' ');
+
+                // check if length is equal to password length
+                let passwordValue = document.querySelector('input[name=password]').value;
+                console.log(passwordValue);
+
+                if(input.value.length > 5)
+                {
+                    if(passwordValue.length === input.value.length)
+                    {
+                        if(passwordValue === input.value)
+                        {
+                            displaySuccess(input, input.nextElementSibling, '');
+                        }
+                    }
+                    else
+                    {
+                        displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} does not match password`);
+                    }
+                }
+                else
+                {
+                    displayError(input, input.nextElementSibling, `${titleCase(nameAttribute)} must be more than 5 characters`);
+                }
+            }
         }
     });
 }
